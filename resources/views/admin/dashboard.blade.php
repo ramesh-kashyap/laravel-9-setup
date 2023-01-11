3937464092e6c65462d09a3329705381f507db62
ramesh-kashyap <75538046+ramesh-kashyap@users.noreply.github.com>
2023-01-11 20:25:51 +0530

    newChange

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,99 +2,172 @@
 <div class="content-wrapper">
 
     <!-- Content -->
+    <style>
+        .card-body {
+            flex: 1 1 auto;
+            padding: 0;
+            color: var(--bs-card-color);
+        }
 
+        .text-muted {
+            --bs-text-opacity: 1;
+            color: #46464a !important;
+            font-size: 1.3125rem;
+            font-weight: 700;
+        }
+        .card {
+            background-clip: padding-box;
+            box-shadow: 0 2.35rem 1.425rem rgb(58 0 255 / 10%);
+            border: 1px solid #d1d1d1;
+        }
+    </style>
     <div class="container-xxl flex-grow-1 container-p-y">
 
 
         <div class="row">
 
-            <!-- Sales last year -->
-
-            <!-- Total Profit -->
-            <div class="col-xl-4 mb-4 col-lg-5 col-12">
+            <!-- total user -->
+            <div class="col-lg-3 col-sm-6 mb-4">
                 <div class="card">
-                    <div class="row">
-                        <div class="col-7">
-                            <div class="card-body text-nowrap">
-                                <h5 class="card-title mb-0">Congratulations John! 🎉</h5>
-                                <p class="mb-2">Best seller of the month</p>
-                                <h4 class="text-primary mb-1">$48.9k</h4>
-
-                            </div>
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Total User</small>
+                            <p class="card-text text-success"><img style="width:56px;" src="{{asset('admin/assets/img/user.png')}}" alt=""></p>
                         </div>
-
+                        <h4 class="card-title mb-1">20</h4>
                     </div>
+                  
                 </div>
             </div>
 
-            <div class="col-xl-3 mb-3 col-lg-5 col-12">
+              <!-- Active user -->
+              <div class="col-lg-3 col-sm-6 mb-4">
                 <div class="card">
-                    <div class="row">
-                        <div class="col-7">
-                            <div class="card-body text-nowrap">
-                                <h5 class="card-title mb-0">Congratulations John! 🎉</h5>
-                                <p class="mb-2">Best seller of the month</p>
-                                <h4 class="text-primary mb-1">$48.9k</h4>
-
-                            </div>
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Active User</small>
+                            <p class="card-text text-success"><img style="width:56px;" src="{{asset('admin/assets/img/user.png')}}" alt=""></p>
                         </div>
-
+                        <h4 class="card-title mb-1">5678</h4>
                     </div>
+                  
                 </div>
             </div>
 
-            <div class="col-xl-3 mb-3 col-lg-5 col-12">
+              <!-- Active user -->
+              <div class="col-lg-3 col-sm-6 mb-4">
                 <div class="card">
-                    <div class="row">
-                        <div class="col-7">
-                            <div class="card-body text-nowrap">
-                                <h5 class="card-title mb-0">Congratulations John! 🎉</h5>
-                                <p class="mb-2">Best seller of the month</p>
-                                <h4 class="text-primary mb-1">$48.9k</h4>
-
-                            </div>
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Pending User</small>
+                            <p class="card-text text-success"><img style="width:56px;" src="{{asset('admin/assets/img/user.png')}}" alt=""></p>
                         </div>
-
+                        <h4 class="card-title mb-1">$42.5k</h4>
                     </div>
+                  
                 </div>
             </div>
 
-            <div class="col-xl-3 mb-3 col-lg-5 col-12">
+              <!-- Todays Registration -->
+              <div class="col-lg-3 col-sm-6 mb-4">
                 <div class="card">
-                    <div class="row">
-                        <div class="col-7">
-                            <div class="card-body text-nowrap">
-                                <h5 class="card-title mb-0">Congratulations John! 🎉</h5>
-                                <p class="mb-2">Best seller of the month</p>
-                                <h4 class="text-primary mb-1">$48.9k</h4>
-
-                            </div>
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Today Registration</small>
+                            <p class="card-text text-success"><img style="width:56px;" src="{{asset('admin/assets/img/user.png')}}" alt=""></p>
                         </div>
-
+                        <h4 class="card-title mb-1">42.5k</h4>
                     </div>
+                  
                 </div>
             </div>
 
 
-            <!-- Total Sales -->
-            <div class="col-xl-2 col-md-4 col-6 mb-4">
+               <!-- Todays Activated -->
+               <div class="col-lg-3 col-sm-6 mb-4">
                 <div class="card">
-                    <div class="card-body">
-                        <div class="badge p-2 bg-label-info mb-2 rounded"><i class="ti ti-chart-bar ti-md"></i></div>
-                        <h5 class="card-title mb-1 pt-2">Total Sales</h5>
-                        <small class="text-muted">Last week</small>
-                        <p class="mb-2 mt-1">$4,673</p>
-                        <div class="pt-1">
-                            <span class="badge bg-label-secondary">+25.2%</span>
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Today Activated</small>
+                            <p class="card-text text-success"><img style="width:56px;" src="{{asset('admin/assets/img/user.png')}}" alt=""></p>
                         </div>
+                        <h4 class="card-title mb-1">42.5k</h4>
                     </div>
+                  
                 </div>
             </div>
 
+             <!-- Total Business -->
+             <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Total Business</small>
+                            <p class="card-text text-success"><img style="width: 36px;" src="{{asset('admin/assets/img/bonus.png')}}" alt=""></p>
+                        </div>
+                        <h4 class="card-title mb-1">{{currency()}}42.5k</h4>
+                    </div>
+                  
+                </div>
+            </div>
 
+            <!-- Total Business -->
+            <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Total Level Bonus</small>
+                            <p class="card-text text-success"><img style="width: 36px;" src="{{asset('admin/assets/img/5110795.png')}}" alt=""></p>
+                        </div>
+                        <h4 class="card-title mb-1">{{currency()}}42.5k</h4>
+                    </div>
+                  
+                </div>
+            </div>
 
+             <!-- Total Business -->
+             <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Total Roi Bonus</small>
+                            <p class="card-text text-success"><img style="width: 36px;" src="{{asset('admin/assets/img/5110795.png')}}" alt=""></p>
+                        </div>
+                        <h4 class="card-title mb-1">{{currency()}}42.5k</h4>
+                    </div>
+                  
+                </div>
+            </div>
 
+              <!-- Total Business -->
+              <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Pending Withdrawal</small>
+                            <p class="card-text text-success"><img style="width: 36px;" src="{{asset('admin/assets/img/bonus.png')}}" alt=""></p>
+                        </div>
+                        <h4 class="card-title mb-1">{{currency()}}42.5k</h4>
+                    </div>
+                  
+                </div>
+            </div>
 
+               <!-- Total Business -->
+               <div class="col-lg-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <small class="d-block mb-1 text-muted">Approved Withdrawal</small>
+                            <p class="card-text text-success"><img style="width: 36px;" src="{{asset('admin/assets/img/bonus.png')}}" alt=""></p>
+                        </div>
+                        <h4 class="card-title mb-1">{{currency()}}42.5k</h4>
+                    </div>
+                  
+                </div>
+            </div>
+
+          
 
         </div>
 
